0ac63f7bae6e0e99086f8498400efd25bdce059a
petertechy <petertechy01@gmail.com>
2025-03-11 13:03:36 +0100

    routes

---
+++ routes/admin.route.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const {
+  addUser,
+  getDashboard,
+  deleteUser,
+  editUser,
+  landingPage,
+  endPoint,
+  signUpPage,
+  aboutPage,
+} = require("../controllers/user.controller");
+const { addProduct } = require("../controllers/product.controller");
+const router = express.Router();
+
+router.get("/about", aboutPage);
+
+router.get("/", landingPage);
+
+router.get("/users", endPoint);
+
+router.get("/signup", signUpPage);
+
+router.post("/register", addUser);
+
+router.get("/dashboard", getDashboard);
+
+router.post("/delete/:id", deleteUser);
+
+router.post("/edit/:id", editUser);
+
+router.post("/add-product", addProduct)
+
+module.exports = router;
