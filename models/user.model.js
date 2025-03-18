c76334d104f53261df1382454ea4431eff1d5614
petertechy <petertechy01@gmail.com>
2025-03-18 12:30:18 +0100

    models user updated

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 let UserSchema = mongoose.Schema({
     firstname: { type: String, required: true },
@@ -8,6 +9,36 @@ let UserSchema = mongoose.Schema({
     password: { type: String, required: true },
     date: { type: Date, default: Date.now() },
   });
+
+  // Using Bycrypt to hash user password
+  let saltRound = 10
+  UserSchema.pre('save', function(next){
+    bcrypt.hash(this.password, saltRound, (err, hashedPassword)=>{
+      // console.log(this.password)
+      // console.log(hashedPassword)
+      if(err){
+        console.log("There is an error setting up password", err)
+      }else{
+        this.password = hashedPassword
+        next()
+      }
+    })
+  })
+
+  //Compare hashed password and user inputted password
+  UserSchema.methods.validatePassword = function(password, callback){
+    console.log(this.password, password)
+    bcrypt.compare(password, this.password, (err, same)=>{
+      if(!err){
+        console.log(same)
+        callback(err, same)
+      }else{
+        next()
+      }
+    })
+  }
+
+  
   
   let UserModel = mongoose.model(
     "user_collection",
